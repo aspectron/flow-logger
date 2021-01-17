ffda27b1dac5d06eb3bb7d0e4b61eafcb07a2bd5
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2021-01-17 12:56:50 -0500

    rename logger.js to flow-logger

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { FlowBasicLogger } = require("./lib/basic-file-logger");
-const { FlowLogger } = require("./lib/logger");
+const { FlowLogger } = require("./lib/flow-logger");
 const { colors, levels } = require("./lib/log-types");
 module.exports = { FlowBasicLogger, FlowLogger, colors, levels };
