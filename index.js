2145fd6fe182445f78ebdcff7a47611581287a46
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2021-01-17 12:56:04 -0500

    logger

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const { FlowBasicLogger } = require("./lib/logger");
-module.exports = { FlowBasicLogger };
+const { FlowBasicLogger } = require("./lib/basic-file-logger");
+const { FlowLogger } = require("./lib/logger");
+const { colors, levels } = require("./lib/log-types");
+module.exports = { FlowBasicLogger, FlowLogger, colors, levels };
