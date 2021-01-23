0d5b800d3185f8163b81437f33f30e91079f76c3
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2021-01-22 23:06:17 -0500

    flow logger

--- test/log-test.ts
+++
@@ -1,8 +0,0 @@
-import  { FlowLogger }  from '../lib/flow-logger'
-
-
-let log = new FlowLogger('TS', { custom : ['utxo:blue']});
-
-log.enable('utxo');
-log.error('hello world');
-log.utxo('hello world');
